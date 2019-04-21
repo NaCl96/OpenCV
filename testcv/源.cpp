7cc6a5a9fbfb930735e6171ddda8ecd4938ccfdb
NaCl96 <13163062832@163.com>
2019-04-21 21:49:10 +0800

    SIFT/SUFT

--- testcv/源.cpp
+++ testcv/源.cpp
@@ -89,116 +89,53 @@ int main() {
 	if (image1.empty()) {
 		cerr <<" Í¼Ïñ¶ÁÈ¡Ê§°Ü" << endl;
 	}
+	// 2. Define keypoints vector
 	std::vector<cv::KeyPoint> keypoints1;
 	std::vector<cv::KeyPoint> keypoints2;
-	cv::Ptr<cv::FeatureDetector> ptrDetector; //·ºÐÍ¼ì²âÆ÷Ö¸Õë
-	ptrDetector = cv::FastFeatureDetector::create(80);
 
-	//¼ì²â¹Ø¼üµã
-	ptrDetector->detect(image1, keypoints1);
-	ptrDetector->detect(image2, keypoints2);
+	// 3. Define feature detector
+	// Construct the SURF feature detector object
+	cv::Ptr<cv::Feature2D> ptrFeature2D = cv::xfeatures2d::SURF::create(2000.0);
+	// to test with SIFT instead of SURF 
+	// cv::Ptr<cv::Feature2D> ptrFeature2D = cv::xfeatures2d::SIFT::create(74);
 
-	//¶¨ÒåÕý·½ÐÎµÄÁÚÓò
-	const int nsize(11); //³ß´ç
-	cv::Rect neighborhood(0, 0, nsize, nsize);
-	cv::Mat patch1;
-	cv::Mat patch2;
+	// 4. Keypoint detection
+	// Detect the SURF features
+	ptrFeature2D->detect(image1, keypoints1);
+	ptrFeature2D->detect(image2, keypoints2);
 
-	cv::Mat result;
+	// Draw feature points
+	cv::Mat featureImage;
+	cv::drawKeypoints(image1, keypoints1, featureImage, cv::Scalar(255, 255, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	// Display the corners
+	cv::namedWindow("SURF");
+	cv::imshow("SURF", featureImage);
+
+	// 5. Extract the descriptor
+	cv::Mat descriptors1;
+	cv::Mat descriptors2;
+	ptrFeature2D->compute(image1, keypoints1, descriptors1);
+	ptrFeature2D->compute(image2, keypoints2, descriptors2);
+
+	//Construction of the matcher 
+	cv::BFMatcher matcher(cv::NORM_L2);
 	std::vector<cv::DMatch> matches;
+	matcher.match(descriptors1, descriptors2, matches);
 
-	for (int i = 0; i < keypoints1.size(); i++) {
-		neighborhood.x = keypoints1[i].pt.x - nsize / 2;
-		neighborhood.y = keypoints1[i].pt.y - nsize / 2;
-		if (neighborhood.x<0 || neighborhood.y<0 || neighborhood.x + nsize>image1.cols || neighborhood.y + nsize>image1.rows) {
-			continue;
-		}
-		patch1 = image1(neighborhood);
-		cv::DMatch bestMatch;
+	cv::Mat imageMatches;
+	cv::drawMatches(image1,keypoints1, // 1st image and its keypoints
+		image2,keypoints2,				// 2nd image and its keypoints
+		matches,imageMatches,			// the matches,the image produced
+		cv::Scalar(255,255,255),cv::Scalar(255,255,255),// color of lines, color of points
+		std::vector<char>(),					// masks if any 
+		cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS | cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-		//for all keypoints in image 2
-		for (int j = 0; j<keypoints2.size(); j++) {
+	// Display the image of matches
+	cv::namedWindow("SURF Matches");
+	cv::imshow("SURF Matches", imageMatches);
 
-			// define image patch
-			neighborhood.x = keypoints2[j].pt.x - nsize / 2;
-			neighborhood.y = keypoints2[j].pt.y - nsize / 2;
-
-			// if neighborhood of points outside image, then continue with next point
-			if (neighborhood.x<0 || neighborhood.y<0 ||
-				neighborhood.x + nsize >= image2.cols || neighborhood.y + nsize >= image2.rows)
-				continue;
-
-			// patch in image 2
-			patch2 = image2(neighborhood);
-
-			// match the two patches
-			cv::matchTemplate(patch1, patch2, result, cv::TM_SQDIFF);
-
-			// check if it is a best match
-			if (result.at<float>(0, 0) < bestMatch.distance) {
-
-				bestMatch.distance = result.at<float>(0, 0);
-				bestMatch.queryIdx = i;
-				bestMatch.trainIdx = j;
-			}
-		}
-		// add the best match
-		matches.push_back(bestMatch);
-	}
 	std::cout << "Number of matches: " << matches.size() << std::endl;
-
-	// extract the 50 best matches
-	std::nth_element(matches.begin(), matches.begin() + 50, matches.end());
-	matches.erase(matches.begin() + 50, matches.end());
-
-	std::cout << "Number of matches (after): " << matches.size() << std::endl;
-
-	// Draw the matching results
-	cv::Mat matchImage;
-	cv::drawMatches(image1, keypoints1, // first image
-		image2, keypoints2, // second image
-		matches,     // vector of matches
-		matchImage,  // produced image
-		cv::Scalar(255, 255, 255),  // line color
-		cv::Scalar(255, 255, 255)); // point color
-
-									// Display the image of matches
-	cv::namedWindow("Matches");
-	cv::imshow("Matches", matchImage);
-
-
-	// define a template
-	cv::Mat target(image1, cv::Rect(80, 105, 30, 30));
-	// Display the template
-	cv::namedWindow("Template");
-	cv::imshow("Template", target);
-
-	// define search region
-	cv::Mat roi(image2,
-		// here top half of the image
-		cv::Rect(0, 0, image2.cols, image2.rows / 2));
-
-	// perform template matching
-	cv::matchTemplate(
-		roi,    // search region
-		target, // template
-		result, // result
-		CV_TM_SQDIFF); // similarity measure
-
-					   // find most similar location
-	double minVal, maxVal;
-	cv::Point minPt, maxPt;
-	cv::minMaxLoc(result, &minVal, &maxVal, &minPt, &maxPt);
-
-	// draw rectangle at most similar location
-	// at minPt in this case
-	cv::rectangle(roi, cv::Rect(minPt.x, minPt.y, target.cols, target.rows), 255);
-
-	// Display the template
-	cv::namedWindow("Best");
-	cv::imshow("Best", image2);
-
-
 	waitKey(0);
 	destroyAllWindows;
 
